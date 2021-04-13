c362e8f6e85b5ba86c144bd4da5bb09c823794bf
Ozan Sülükpınar <ozansulukpinar@hotmail.com>
2021-04-14 02:28:05 +0300

    Add files via upload

---
+++ OneWebPage/main.js
@@ -0,0 +1,22 @@
+function assignColours() {
+    var rgb = [];
+    for (k = 0; k < 3; k++) {
+        var rgbNumber = Math.floor(Math.random() * 256);
+        rgb[k] = rgbNumber;
+    }
+    return rgb;
+}
+
+for (i = 0; i < 4; i++) {
+    var rgbNumbers = assignColours();
+    $('#' + i).css({ 'background-color': 'rgb(' + rgbNumbers[0] + ',' + rgbNumbers[1] + ',' + rgbNumbers[2] + ')' });
+}
+
+function Click() {
+    $('#0').delay(1000).fadeOut();
+    $('#1').delay(750).fadeOut();
+    $('#2').delay(500).fadeOut();
+    $('#3').delay(250).fadeOut();
+}
+
+$('#0, #1, #2, #3').on('click', Click);
